ef4d6b679a4c916b7072b13363fa1b6b3f7ed18d
leo <leokastenberg@gmail.com>
2018-02-02 22:53:18 -0500

    sorry, last one didn't work

---
+++ grapher.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open("output.txt","r") as f:
+    inpt = f.read().split()
+for i in range(len(inpt)):
+    inpt[i] = float(inpt[i])
+
+x = np.linspace(-5, 7, 100)
+
+plt.plot([inpt[2*i+1] for i in range( (len(inpt) - 4) / 2)], [inpt[2*i+2] for i in range( (len(inpt) - 4) / 2 )],'ro')
+plt.plot(x, (inpt[-3]*x)**2 + inpt[-2]*x + inpt[-1], label = 'graph')
+plt.xlabel('x label')
+plt.ylabel('y label')
+
+plt.title("Simple Plot")
+
+plt.legend()
+
+plt.show()
